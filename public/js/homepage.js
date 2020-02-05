9ed1258208751cbbf6e7b83800a9600546edbf30
kimiinglet <kinglet91391@gmail.com>
2020-02-05 14:13:18 -0600

    pulling

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,4 +1,3 @@
-$(document).ready(function() {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
@@ -36,4 +35,6 @@ $(document).ready(function() {
         console.log(err);
       });
   }
-});
+
+
+
