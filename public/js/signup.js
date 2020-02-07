5a911e478d7eb1ec600924eeedf95f9f4b60f3b4
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-02-07 12:56:02 -0600

    travis errors resolved

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var usernameInput = $("input#username-input");
+  var signUpForm = $("form.signup")
+  var usernameInput = $("input#username-input")
   // var emailInput = $("input#email-input");
   // var passwordInput = $("input#password-input");
 
@@ -12,29 +12,29 @@ $(document).ready(function () {
       username: usernameInput.val().trim()
       // email: emailInput.val().trim(),
       // password: passwordInput.val().trim()
-    };
+    }
 
     if (!userData.username) {
       // (!userData.email || !userData.password)
-      return;
+      return
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.username);
+    signUpUser(userData.username)
     // (userData.email, userData.password)
-    usernameInput.val("");
+    usernameInput.val("")
     // emailInput.val("");
     // passwordInput.val("");
-  });
+  })
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
   function signUpUser(username) {
     // (email, password)
     $.post("/api/signup", {
-      username: username
-      // email: email,
-      // password: password
-    })
+        username: username
+        // email: email,
+        // password: password
+      })
       .then(function () {
         window.location.replace("/members")
         // If there's an error, handle it by throwing up a bootstrap alert
--- public/js/user.js
+++ public/js/user.js
@@ -2,32 +2,32 @@
 
 // When user clicks add-btn
 $("#user-submit").on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
     // Make a newUser object
     var newUser = {
         name: $("#user").val().trim(),
         created_at: moment().format("YYYY-MM-DD HH:mm:ss")
-    };
+    }
 
-    console.log(newUser);
+    console.log(newUser)
 
     // Send an AJAX POST-request with jQuery
     $.post("/api/new", newUser)
         // On success, run the following code
         .then(function () {
 
-            var row = $("<div>");
-            row.addClass("user");
+            var row = $("<div>")
+            row.addClass("user")
 
-            row.append("<p>" + newUser.name + " added: </p>");
-            row.append("<p>At " + moment(newUser.created_at).format("h:mma on dddd") + "</p>");
+            row.append("<p>" + newUser.name + " added: </p>")
+            row.append("<p>At " + moment(newUser.created_at).format("h:mma on dddd") + "</p>")
 
-        });
+        })
 
     // Empty each input box by replacing the value with an empty string
-    $("#user").val("");
-});
+    $("#user").val("")
+})
 
 // When the page loads, grab all of our users
 $.get("/api/all", function (data) {
@@ -36,13 +36,13 @@ $.get("/api/all", function (data) {
 
         for (var i = 0; i < data.length; i++) {
 
-            var row = $("<div>");
-            row.addClass("user");
+            var row = $("<div>")
+            row.addClass("user")
 
-            row.append("<p>" + data[i].user + " added.. </p>");
-            row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+            row.append("<p>" + data[i].user + " added.. </p>")
+            row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>")
         }
 
     }
 
-});
+})
