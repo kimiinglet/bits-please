8b0ff6ba390e43714d38d4553589ace7f1722383
Brad Bryant <bradleybryant86@gmail.com>
2020-02-07 12:54:45 -0600

    Began get function to allow username to follow user to quiz page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,39 +1,46 @@
 $(document).ready(function () {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var usernameInput = $("input#username-input");
-  // var emailInput = $("input#email-input");
-  // var passwordInput = $("input#password-input");
+  var signUpForm = $("form.signup")
+  // var usernameInput = $("input#username-input")
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
 
-  // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function (event) {
+  // When the signup button is clicked, we check to make sure a username has been made
     event.preventDefault()
     var userData = {
-      username: usernameInput.val().trim()
-      // email: emailInput.val().trim(),
-      // password: passwordInput.val().trim()
-    };
+      // username: usernameInput.val().trim()
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim()
+    }
 
-    if (!userData.username) {
-      // (!userData.email || !userData.password)
+    if (!userData.email || !userData.password) {
+      (!userData.username)
       return;
     }
-    // If we have an email and password, run the signUpUser function
-    signUpUser(userData.username);
-    // (userData.email, userData.password)
-    usernameInput.val("");
-    // emailInput.val("");
-    // passwordInput.val("");
-  });
+    // If we have a username, run the signUpUser function
+    signUpUser(userData.email, userData.password);
+    // (userData.username)
+    emailInput.val("");
+    passwordInput.val("");
+    //usernameInput.val("");
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////Get username to follow user through the quiz//////////////////////////////
+  //Get references to our sign-up form and input
+
+  $.get("/api/user_data").then(function(data) {
+    $(".account_hero_name").text(data.username);
+  })
+  })
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(username) {
-    // (email, password)
+  function signUpUser (email, password) {
+   //(username)
     $.post("/api/signup", {
-      username: username
-      // email: email,
-      // password: password
+      email: email,
+      password: password
+      //username: username
     })
       .then(function () {
         window.location.replace("/members")
@@ -47,3 +54,5 @@ $(document).ready(function () {
     $("#alert").fadeIn(500)
   }
 })
+
+
